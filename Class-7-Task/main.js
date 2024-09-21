dc9c7189c8da13cfdd82781ec4d73e4c8368292f
abhin <abhin.pik@gmail.com>
2024-09-21 12:36:18 +0100

    Clss 7 task

---
+++ Class-7-Task/main.js
@@ -0,0 +1,14 @@
+
+function changeMood (mood) {
+    let moods = {
+        happy: "&#128514;",
+        smile: "&#128513;",
+        cry: "&#128557;"
+    }
+
+    document.getElementById('showMood').innerHTML = moods[mood];
+}
+
+document.getElementById('makeHappy').addEventListener("click", (e) => changeMood('happy'));
+document.getElementById('makeSmile').addEventListener("click", (e) => changeMood('smile'));
+document.getElementById('makeCry').addEventListener("click", (e) => changeMood('cry'));
